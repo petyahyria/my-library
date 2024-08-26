f45d0b4296591b555fc0d6807472ed7d850dd462
peter <112580184+petyahyria@users.noreply.github.com>
2024-08-26 19:33:02 +0300

    Add new book dialog and style it

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ function Book(title, author, pages, readStatus){
 Book.prototype.info = function() {
     return `${title} by ${author}, ${pages} pages, ${readStatus ? "read" : "not read yet"}`;
 }
+
+const addBookToLibrary = () => {
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const readCheckbox = document.querySelector("#read");
+
+    
+}
